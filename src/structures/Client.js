aaeabd308e1a76169ac273f0bb2829ec0d93ccd1
DylanOfDiscord <72952471+DylanOfDiscord@users.noreply.github.com>
2022-10-17 22:49:50 -0400

    Update Client.js

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -70,7 +70,7 @@ module.exports = class DGH_BOT_CLIENT extends Client {
     const { init } = require("./mongoose");
     require("./verifyConfig")(this.config);
     init();
-    super.login(process.env.token);
+    super.login(this.config.bot.token);
     this.on("ready", async () => {
       console.log(`Bot Is Ready To Go!\nTag: ${this.user.tag}`);
       this.channels.cache.get(logs.boton).send({
